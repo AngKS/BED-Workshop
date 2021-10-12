d3781008c3867aab172cef9a865b0c6a9d597e1b
AKS <60446729+AngKS@users.noreply.github.com>
2021-10-12 15:25:13 +0800

    Added GET all Users endpoint

--- sourceCode/myFirstWS/controller/app.js
+++ sourceCode/myFirstWS/controller/app.js
@@ -5,6 +5,20 @@ const app = express()
 
 // API Endpoints
 
+// Endpoint to get ALL users
+app.get('/api/users', (req, res) => {
+    
+    User.getAllUsers((err, result) => {
+        if (!err){
+            res.status(200).send(result)
+        }
+        else{
+            res.status(500).send("Internal Server Error")
+        }
+    })
+})
+
+// Endpoint to get specific user by UserID
 app.get('/api/user/:userID', (req, res) => {
     let userID = req.params.userID
 
--- sourceCode/myFirstWS/model/user.js
+++ sourceCode/myFirstWS/model/user.js
@@ -3,12 +3,35 @@ const db = require("./databaseConfig")
 
 let User = {
 
+    getAllUsers: (callback) => {
+        let conn = db.getConnection()
+        conn.connect((err) => {
+            if (err) {
+                return callback(err, null)
+            }
+            else {
+                console.log('Database Connected')
+                let QUERY = `SELECT * FROM Users`
+                conn.query(QUERY, (err, result) => {
+                    conn.end()
+                    if (err) {
+                        console.log('Query Error')
+                        return callback(err, null)
+                    }
+                    else {
+                        console.log('Query Success!')
+                        return callback(null, result)
+
+                    }
+                })
+            }
+        })
+    },
+
     getUser: (id, callback) => {
         let conn = db.getConnection()
         conn.connect((err) => {
             if (err) {
-                console.log(err)
-                
                 return callback(err, null)
             }
             else {
